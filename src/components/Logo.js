d761eafa32d5ad8553f06b8e98ee170a0efea50e
Valentin Virzi <v.virzi@hotmail.com>
2023-10-03 20:25:50 -0300

    loho href warning

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Logo.css";
 
 const Logo = () => {
   return (
-    <a className="nav__logo-a" href="#">
+    <Link className="nav__logo-a" to="/">
       <img className="nav__logo-img" src="./logo.png" alt="Logo"></img>
-    </a>
+    </Link>
   );
 };
 
