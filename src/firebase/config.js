55f22673d77a3ef34c7321d10aea5ad4a4cca199
Valentin Virzi <v.virzi@hotmail.com>
2023-10-14 13:30:44 -0300

    firebase js

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBNCoaWJRj3Sb42DvzrNVRI1bq_nwTegJE",
+  authDomain: "ecommerce-react-js-coderhouse.firebaseapp.com",
+  projectId: "ecommerce-react-js-coderhouse",
+  storageBucket: "ecommerce-react-js-coderhouse.appspot.com",
+  messagingSenderId: "113409505579",
+  appId: "1:113409505579:web:bb10ab94f810a0c02eed6e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
